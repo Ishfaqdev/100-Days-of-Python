25683ed08c977cef9e5878f002ee94d4bc710b8f
Ishfaqdev <Ishfaqbcs@gmail.com>
2024-01-19 17:14:05 +0500

    data type exercise has been modified

--- Day 2/dataTypeExercise.py
+++ Day 2/dataTypeExercise.py
@@ -1,6 +1,17 @@
-two_digit_number = input("Type a two digit number: ")
+# datatype exercise
+
+two_digit_number = input("Enter a two-digit number: ")
+
 if len(two_digit_number) == 2 and two_digit_number.isdigit():
-    digit_sum = int(two_digit_number[0]) + int(two_digit_number[1])
-    print(f"The sum of the digts in {two_digit_number} is {digit_sum}")
+    sum_digit = int(two_digit_number[0]) + int(two_digit_number[1])
+    print(f"Sum of digits in {two_digit_number} is {sum_digit}")
 else:
-    print("Please enter a valid 2-digit number")
+    print("Enter a valid digit number")
+
+
+# another method
+
+# first_digit = int(two_digit_number[0])
+# second_digit = int(two_digit_number[1])
+
+# print(first_digit + second_digit)
