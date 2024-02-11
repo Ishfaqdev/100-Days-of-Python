e7bed0fb7ddf946b8abd502c0477804fe147d601
Ishfaqdev <Ishfaqbcs@gmail.com>
2024-02-11 12:29:35 +0500

    Day 6 started

---
+++ Day 6/function.py
@@ -0,0 +1,31 @@
+
+# Simple function
+"""
+def greet():
+    print("Hello")
+    print("How are you?")
+    print("Isn't the weather nice today?")
+
+
+greet()
+
+"""
+
+# Function with parameter and arguments
+"""
+def greet_with_param(name):
+    print(f"Hello {name}")
+
+
+greet_with_param("Ishfaq Ahmad")
+
+"""
+
+# Function with two parameter and arguments
+
+
+def greet_two_param(name, city):
+    print(f"Hello my name is {name} and i live in {city}")
+    
+    
+greet_two_param("Ishfaq Ahmad", "Lahore")
